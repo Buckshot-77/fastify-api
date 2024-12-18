7ebd8770d84873a71ffb72822901ab5f21a08809
Hugo Oliveira <hugoooliveira.dev@proton.me>
2024-12-18 09:47:12 -0300

    feat: add route to create transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,13 +1,33 @@
+import { randomUUID } from 'node:crypto'
+
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 
 import { knex } from '../database'
 
 export async function transactionRoutes(app: FastifyInstance) {
-    app.get('/hello', async () => {
-        const transactions = await knex('transactions')
-            .where('amount', 1000)
-            .select('*')
+    app.post('/', async (request, reply) => {
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['debit', 'credit']),
+        })
 
-        return transactions
+        const { title, amount, type } = createTransactionBodySchema.parse(
+            request.body,
+        )
+
+        const amountToReturnObject = {
+            credit: amount,
+            debit: amount * -1,
+        }
+
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: amountToReturnObject[type],
+        })
+
+        return reply.status(201).send()
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.register(transactionRoutes)
+app.register(transactionRoutes, { prefix: 'transactions' })
 
 app.listen({ port: parsedEnv.PORT }, () => {
     console.log(`App listening on port ${parsedEnv.PORT}`)
