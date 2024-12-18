e76fb41b1fd1f0edf65e4bec4980975ddf1e6662
Hugo Oliveira <hugoooliveira.dev@proton.me>
2024-12-17 22:03:58 -0300

    feat: add table for transactions

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import fastify from 'fastify'
+
 import { parsedEnv } from './utils/env'
 
-import { knex } from './database'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/', async () => {
-    const tables = await knex('sqlite_schema').select('*')
-
-    return tables
-})
+app.register(transactionRoutes)
 
 app.listen({ port: parsedEnv.PORT }, () => {
     console.log(`App listening on port ${parsedEnv.PORT}`)
