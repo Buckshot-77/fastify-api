b56edd7955583f550946b935b26828f8ed19254b
Hugo Oliveira <hugoooliveira.dev@proton.me>
2024-12-17 22:15:29 -0300

    feat: add node_env to env file

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,6 +3,9 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
+    NODE_ENV: z
+        .enum(['development', 'test', 'production'])
+        .default('development'),
     PORT: z.number({ coerce: true }).positive(),
     DATABASE_URL: z.string(),
     MIGRATION_PATH: z.string(),
@@ -11,8 +14,13 @@ const envSchema = z.object({
 const parsedEnv = envSchema.safeParse(process.env)
 
 if (parsedEnv.success !== true) {
+    console.error(
+        'There were some problems parsing the env for the application',
+        parsedEnv.error.format(),
+    )
+
     throw new Error(
-        `There were some problems parsing the env for the application: ${parsedEnv.error}`,
+        'There were some problems parsing the env for the application',
     )
 }
 
